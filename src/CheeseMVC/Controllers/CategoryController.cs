deb5f52b34a528e777ea1e31a31dd621262c7840
Silveiro Gonzalez <sgoolea1993@gmail.com>
2018-03-11 05:05:34 -0500

    Ready for demo.

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        //to do add catgory 
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCatergoryViewModel = new AddCategoryViewModel();
+            return View(addCatergoryViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+
+            return View(addCategoryViewModel);
+        }
+    }
+}
